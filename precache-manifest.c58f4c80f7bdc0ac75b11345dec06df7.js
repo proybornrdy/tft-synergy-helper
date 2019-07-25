320d617712a90d4332c2d16e969e4bee7b26fef3
Seung-gyu Lee <seungq92@gmail.com>
2019-07-24 20:50:08 -0400

    Updates

---
+++ precache-manifest.c58f4c80f7bdc0ac75b11345dec06df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa8febbdc9871b223567178efdcefd7",
+    "url": "/tft-synerge-helper/index.html"
+  },
+  {
+    "revision": "c70a26d83ac8ef268aea",
+    "url": "/tft-synerge-helper/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/tft-synerge-helper/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c70a26d83ac8ef268aea",
+    "url": "/tft-synerge-helper/static/js/main.2fc87c95.chunk.js"
+  },
+  {
+    "revision": "74a7315de4026612dfd5",
+    "url": "/tft-synerge-helper/static/js/runtime~main.5f4a9cda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tft-synerge-helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
